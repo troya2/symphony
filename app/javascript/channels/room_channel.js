096e05486bc2d21315caf3a2d94c18c573f07ff2
Troy Anderson <troy@alliedcode.com>
2019-11-02 18:45:10 -0700

    Got action cable properly set up and working for chat.

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,12 +1,14 @@
+import consumer from "./consumer"
+
 $(function() {
   $('[data-channel-subscribe="room"]').each(function(index, element) {
     var $element = $(element),
-        room_id = $element.data('room-id')
+        room_id = $element.data('room-id'),
         messageTemplate = $('[data-role="message-template"]');
 
     $element.animate({ scrollTop: $element.prop("scrollHeight")}, 1000)
 
-    App.cable.subscriptions.create(
+    consumer.subscriptions.create(
       {
         channel: "RoomChannel",
         room: room_id
@@ -15,6 +17,7 @@ $(function() {
         received: function(data) {
           var content = messageTemplate.children().clone(true, true);
           content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
+          content.find('[data-role="user-display-name"]').text(data.user_display_name);
           content.find('[data-role="message-text"]').text(data.message);
           content.find('[data-role="message-date"]').text(data.updated_at);
           $element.append(content);
--- app/models/room_message.rb
+++ app/models/room_message.rb
@@ -2,7 +2,9 @@ class RoomMessage < ApplicationRecord
   belongs_to :room, inverse_of: :room_messages
   belongs_to :user
 
+  delegate :gravatar_url, :display_name, to: :user, prefix: true
+
   def as_json(options = {})
-  	super(options).merge(user_avatar_url: user.gravatar_url)
+  	super(options).merge(user_avatar_url: user_gravatar_url, user_display_name: user_display_name)
   end
 end
